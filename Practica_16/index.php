73d82df3bc2e4b8890b1a0006ab13d7039179ca9
Gerardo Grajales <el_gera_one@hotmail.com>
2012-10-23 19:58:08 -0500

    Practica 16

---
+++ Practica_16/index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html class="no-js">
+
+	<head>
+		<meta charset="utf-8"/>
+		<title>HackerGarage :: Eventos</title>
+        <?php
+			include("sections/header.html");
+        ?>
+	</head>
+	
+	
+	<body class="home">
+	
+		<!-- HEADER -->
+		<header>
+			<?php
+				include("sections/slider.html");
+			?>
+		</header>
+		<!-- ENDS HEADER -->
+		
+		<!-- MAIN -->
+		<div id="main">
+			<?php
+				include("sections/main_1.html");
+			?>
+		</div>
+		<!-- ENDS MAIN -->
+		
+		
+		<!-- FOOTER -->
+		<footer>
+			<?php 
+				include("sections/footer.php");
+			?>
+		</footer>
+		<!-- ENDS FOOTER -->
+		
+	</body>
+	
+	
+	
+</html>
